5b4a7113b5068d146560d8c66118b40f1e88b6fa
Melissa <melissainui@gmail.com>
2021-07-25 14:08:09 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,44 +1,80 @@
-import java.util.Scanner;
+import java.util.*;
+import java.time.LocalDate;
+
 
 public class Main{
 
-    void help(){
-        System.out.println("Escolha o comando desejado:");
-        System.out.println("Adicionar sala de reunião - Digite: S");
-        System.out.println("Remover sala de reunião - Digite: R");
-        System.out.println("Reservar sala de reunião - Digite: E");
-        System.out.println("Cancelar sala de reunião - Digite: C");
-        System.out.println("Imprimir as reservas de sala existentes - Digite: I");
-        System.out.println("Criar participante - Digite: P");
-        System.out.println("Marcar reuniao - Digite: M");
-        System.out.println("Registrar disponibilidade - Digite: D");
-        System.out.println("Mostrar sobreposicao de horários - Digite: O");
-        System.out.println("Ver os comandos disponiveis novamente - Digite: H");
-        System.out.println("Finalizar o programa: F");
+    public static void help(){
+        System.out.println("============== Escolha o comando desejado ================");
+        System.out.println("> Adicionar sala de reunião - Digite: S");
+        System.out.println("> Remover sala de reunião - Digite: R");
+        System.out.println("> Reservar sala de reunião - Digite: E");
+        System.out.println("> Cancelar sala de reunião - Digite: C");
+        System.out.println("> Imprimir as reservas de sala existentes - Digite: I");
+        System.out.println("> Adicionar um participante na reuniao - Digite: P");
+        System.out.println("> Marcar reuniao - Digite: M");
+        System.out.println("> Registrar disponibilidade - Digite: D");
+        System.out.println("> Mostrar sobreposicao de horários - Digite: O");
+        System.out.println("> Ver os comandos disponiveis novamente - Digite: H");
+        System.out.println("> Finalizar o programa: F");
+        System.out.println("===========================================================");
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Collection <String> listaDeParticipantes = new LinkedList<>();
+        MarcadorDeReuniao marcador = new MarcadorDeReuniao();
+        LocalDate inicio_reuniao;
+        LocalDate fim_reuniao;
+
         String comando = input.nextLine();
         help();
-        switch(comando) {
-            case "S": pensarEmComoUsar(); break;
-            case "R": pensarEmComoUsar(); break;
-            case "E": pensarEmComoUsar(); break;
-            case "C": pensarEmComoUsar(); break;
-            case "I": pensarEmComoUsar(); break;
-            case "M": pensarEmComoUsar(); break;
-            case "O": pensarEmComoUsar(); break;
-            case "P": pensarEmComoUsar(); break;
-            case "H": help(); break;
-            case "F": return; 
-            default:
-                System.out.println("ERRO: Insira um comando valido");
-                help();
+
+        while(!comando.equals("F")){
+            switch(comando) {
+                // case "S": pensarEmComoUsar(); break;
+                // case "R": pensarEmComoUsar(); break;
+                // case "E": pensarEmComoUsar(); break;
+                // case "C": pensarEmComoUsar(); break;
+                case "I": 
+                            System.out.print(">>> Digite o nome do participante: ");
+                            String nome_participante = input.nextLine();
+                            System.out.print("\n>>> Horario INICIAL de disponibilidade (dd/mm/yyyy - hh:mm:ss): ");
+                            
+                            // marcador.indicaDisponibilidade(participante, inicio, fim);
+                            break;
+                case "M":
+                            System.out.println("======== ESCOLHA UM HORARIO PARA A REUNIAO ========");
+                            marcador.mostraSobreposicao();
+                            System.out.println("===================================================");
+                            System.out.print("Horario de inicio da reuniao (dd/mm/yyyy): ");
+                            String inicio = input.nextLine();
+                            System.out.print("\nHorario de termino da reuniao (dd/mm/yyyy): ");
+                            String fim = input.nextLine();
+                            System.out.println();
+                            String[]separa_inicio = inicio.split("/");
+                            String[]separa_fim = inicio.split("/");
+                            inicio_reuniao = LocalDate.of(Integer.parseInt(separa_inicio[2]), Integer.parseInt(separa_inicio[1]), Integer.parseInt(separa_inicio[0]));
+                            fim_reuniao = LocalDate.of(Integer.parseInt(separa_fim[2]), Integer.parseInt(separa_fim[1]), Integer.parseInt(separa_fim[0]));
+                            //of(int year, int month, int dayOfMonth)
+                            marcador.marcarReuniaoEntre(inicio_reuniao, fim_reuniao, listaDeParticipantes);
+                            break;
+                // case "O": pensarEmComoUsar(); break;
+                // case "P": pensarEmComoUsar(); break;
+                // case "H": help(); break;
+        
+                default:
+                    System.out.println("ERRO: O comando inserido é invalido. Por favor, digite um comando válido.");
+                    help();
+                    System.out.print("Comando: ");
+                    comando = input.nextLine();
+                    System.out.println();
+                    break;
+                
                 comando = input.nextLine();
-                break;
-          comando = input.nextLine();
+            }
         }
+
 
         //   - Identificador: ID? -> 6 numeros. Verificar se tem repetido, ou 000000, ou negativo
 
